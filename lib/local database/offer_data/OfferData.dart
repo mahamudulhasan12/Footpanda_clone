054e6259152b7adf3026ba7eb80282801370c967
mahamudulhasan12 <mahamudulhasanmunna459@gmail.com>
2026-08-10 18:51:07 +0600

    homes

---
+++ lib/local database/offer_data/OfferData.dart
@@ -0,0 +1,46 @@
+class Offerdata {
+  static List offer=[
+    {
+      "image":"https://cdn-icons-png.flaticon.com/512/6188/6188592.png",
+      "text":"Offers"
+    },
+    {
+      "image":"https://cdn-icons-png.flaticon.com/128/272/272186.png",
+      "text":"New restaurants"
+    },
+    {
+      "image":"https://cdn-icons-png.flaticon.com/128/2936/2936964.png",
+      "text":"Pick-up"
+    },
+    {
+      "image":"https://static.vecteezy.com/system/resources/thumbnails/024/283/039/small/flat-style-colorful-award-badge-icon-vector.jpg",
+      "text":"Super Restaurants"
+    },
+    {
+      "image":"https://cdn-icons-png.flaticon.com/512/6188/6188592.png",
+      "text":"Offers"
+    },
+  ];
+  static List food =[
+    {
+      "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZJFi8pYVVwjRWr_f85Q7qTCxXQQUnb126Yw&s",
+      "text":"Pizza"
+    },
+    {
+      "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcBlpPJMH6jZgmhCX6UXqeChwed0Sf7WYT-Q&s",
+      "text":"Biryani"
+    },
+    {
+      "image":"https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/tk%2Fphoto%2F2025%2F06-2025%2F2025-06-burger-doneness-guide%2Fburger-doneness-guide-0195",
+      "text":"Burgers"
+    },
+    {
+      "image":"https://static.vecteezy.com/system/resources/thumbnails/024/283/039/small/flat-style-colorful-award-badge-icon-vector.jpg",
+      "text":"Fast Food"
+    },
+    {
+      "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1I9CvDB_A40JIL5-YjmKd8Ue8a0ggprvlrQ&s",
+      "text":"Chicken"
+    },
+  ];
+}
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:foodpanda/custom%20widget/app_continner.dart';
 import 'package:foodpanda/custom%20widget/app_text.dart';
 import 'package:foodpanda/custom%20widget/app_textfield.dart';
+import 'package:foodpanda/identity/login/login_screen.dart';
+import 'package:foodpanda/local%20database/offer_data/OfferData.dart';
 import 'package:foodpanda/util/app_colors.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -13,21 +16,24 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController search = TextEditingController();
-  List image =[
+  int cPage = 0;
+  List image = [
     {
       "images": "https://cdn-icons-png.flaticon.com/128/8596/8596040.png",
-      "text":"Here's 50% off & free \ndelivery on your first order !",
+      "text": "Here's 50% off & free \ndelivery on your first order !",
     },
     {
       "images": "https://cdn-icons-png.flaticon.com/128/3063/3063385.png",
-      "text":"Here's 60% off & free \ndelivery on your first order !",
+      "text": "Here's 60% off & free \ndelivery on your first order !",
     },
-
-
   ];
+
+  List data = Offerdata.offer;
+  List foods =Offerdata.food;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.pColros,
       appBar: AppBar(
         backgroundColor: AppColors.pColros,
         leading: IconButton(
@@ -62,9 +68,9 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.all(12.0),
             child: AppTextField(
               controller: search,
-              lText: AppText(text: "Search for restaurants",fontSize: 15,),
+              lText: AppText(text: "Search for restaurants", fontSize: 15),
               isDense: true,
-              pIcons: Icon(Icons.search_rounded,size: 30,),
+              pIcons: Icon(Icons.search_rounded, size: 30),
               borderRadius: BorderRadius.circular(25),
               borderSide: BorderSide.none,
               fillColor: AppColors.sColors,
@@ -75,58 +81,206 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: ListView(
         children: [
-          CarouselSlider(items: image.map((i){
-            return Builder(builder: (BuildContext context){
-              return Padding(
-                padding:EdgeInsets.symmetric(),
-                child: Container(
-                  padding: EdgeInsets.only(left: 10,right: 10,top: 10),
+          CarouselSlider(
+            items: image.map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(),
+                    child: Container(
+                      padding: EdgeInsets.only(left: 10, right: 10, top: 10),
 
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    // borderRadius: BorderRadius.circular(5),
-                    color: AppColors.pColros
-                  ),
-                  child: Row(
-                    spacing: 20,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        // borderRadius: BorderRadius.circular(5),
+                        color: AppColors.pColros,
+                      ),
+                      child: Stack(
                         children: [
-                          AppText(text: "${i["text"]}",fontSize: 16,fontWeight: FontWeight.bold,colors: Colors.white,),
-                          TextButton(onPressed: (){}, child: AppText(text: "Start ordering",fontSize: 10,colors: Colors.white,worldSpacing: 2,))
+                          Row(
+                            spacing: 20,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  AppText(
+                                    text: "${i["text"]}",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    colors: Colors.white,
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: AppText(
+                                      text: "Start ordering",
+                                      fontSize: 10,
+                                      colors: Colors.white,
+                                      worldSpacing: 2,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Image.network(
+                                height: 100,
+                                width: 80,
+                                "${i['images']}",
+                              ),
+                            ],
+                          ),
+                          Positioned(
+                            bottom: 5,
+                            left: 150,
+                            child: buildCarasoulIndicator(),
+                          ),
                         ],
                       ),
-                      Image.network(
-                        height: 100,
-                          width: 80,
-                          "${i['images']}"
-                      ),
-                    ],
-                  )
-                ),
+                    ),
+                  );
+                },
               );
-            });
-          }).toList(), options: CarouselOptions(
-            height: 110,
+            }).toList(),
+            options: CarouselOptions(
+              height: 110,
 
-            aspectRatio: 0.99,
-            viewportFraction: .99,
-            initialPage: 0,
-            enableInfiniteScroll: true,
-            reverse: false,
-            autoPlay: true,
-            autoPlayInterval: Duration(seconds: 30),
-            // autoPlayAnimationDuration: Duration(milliseconds: 800),
-            // autoPlayCurve: Curves.fastOutSlowIn,
-            // enlargeCenterPage: true,
-            // enlargeFactor: 0.3,
+              aspectRatio: 0.99,
+              viewportFraction: .99,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 5),
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (value, _) {
+                setState(() {
+                  cPage = value;
+                });
+              },
+            ),
+          ),
+          AppContinner(
+            pading: EdgeInsets.only(left: 15, right: 15, top: 30),
+            width: MediaQuery.sizeOf(context).width,
+            height: 800,
+            borderRadius: BorderRadius.circular(20),
+            colors: Colors.white,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
 
-            scrollDirection: Axis.horizontal,
-          ))
-         ]
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: data.length,
+                      itemBuilder: (context , index){
+                    return AppContinner(
+                      height: 90,
+                      width: 90,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            height: 50,
+                            width: 50,
+                            "${data[index]['image']}",
+                          ),
+                          AppText(text: "${data[index]['text']}",fontWeight: FontWeight.bold,overflow:TextOverflow.ellipsis,),SizedBox(height: 5,),
+                          Divider(thickness: 1,color: Colors.grey.shade300,),
+
+                        ],
+                      ),
+                    );
+                  }),
+                ),
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: foods.length,
+                      itemBuilder: (context , index){
+                        return AppContinner(
+                          height: 90,
+                          width: 90,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Column(
+                            children: [
+                              Image.network(
+                                height: 50,
+                                width: 50,
+                                "${foods[index]['image']}",
+                              ),
+                              AppText(text: "${foods[index]['text']}",overflow:TextOverflow.ellipsis,),SizedBox(height: 5,),
+
+                            ],
+                          ),
+                        );
+                      }),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppText(text: "Popular Restaurants",fontWeight: FontWeight.bold,colors: AppColors.tColors,fontSize: 17,),
+                    InkWell(
+                      onTap: (){},
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          // color: Colors.blue,
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 1,color: Colors.grey),
+                        ),
+                        child: Icon(Icons.arrow_forward_ios_outlined,size: 20,),
+                      ),
+                    )
+
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        onTap: (index){
+          setState(() {
+            HomeScreen();
+            LoginScreen();
+          });
+        },
+        backgroundColor: Colors.white,
+        selectedItemColor: AppColors.pColros,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Food"),
+          BottomNavigationBarItem(icon: Icon(Icons.local_grocery_store),label: "Grocery"),
+          BottomNavigationBarItem(icon: Icon(Icons.search_outlined),label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.card_travel_sharp),label: "Carts",),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline),label: "Accounts"),
+
+        ],
+      ),
+    );
+  }
+
+  buildCarasoulIndicator() {
+    return Row(
+      children: [
+        for (int i = 0; i < image.length; i++)
+          Container(
+            margin: EdgeInsets.all(5),
+            height: i == cPage ? 8 : 5,
+            width: i == cPage ? 30 : 5,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              // shape: BoxShape.circle,
+              color: i == cPage ? Colors.blue : Colors.grey,
+            ),
+          ),
+      ],
     );
   }
 }
